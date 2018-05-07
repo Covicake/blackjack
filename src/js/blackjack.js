7a6575c6a0fea6fff97f04f58e43b8b74656addb
Fernando <fernand-o.a@hotmail.com>
2018-05-07 15:01:30 +0200

    Anadido Lodash

--- src/js/blackjack.js
+++ src/js/blackjack.js
@@ -1,3 +1,4 @@
+//Elementos HTML que se modifican en tiempo de ejecucion.
 let elems = {
   botonCarta: document.querySelector(".botonCarta"),
   botonPlantar: document.querySelector(".botonPlantar"),
@@ -9,23 +10,14 @@ let elems = {
 elems.botonCarta.onclick = repartir;
 elems.botonPlantar.onclick = plantarse;
 
-/**Clase que representa una Carta */
+
 class Carta{
-  /**
-   * Crea una Carta.
-   * @param {Number} valor Valor numérico de la carta.
-   * @param {String} palo El palo al que pertenece la carta.
-   */
   constructor(valor, palo){
       this.valor = valor;
       this.palo = palo;
   }
 
-  /**
-   * Metodo para conseguir un simbolo que representa el palo de la carta
-   * @return {String}
-   */
-  getSymbol(){
+  getSymbol(){  //Devuelve un simbolo en funcion del palo de la carta.
     let symbol;
     switch(this.palo){
       case "C":
@@ -45,77 +37,48 @@ class Carta{
   }
 }
 
-/**Clase que representa una baraja de cartas */
-class Baraja{
-  /**
-   * Crea un contenedor para la baraja.
-   */
+class Baraja{  //Construye y almacena un array de objetos Carta.
   constructor(){
     this.baraja = [];
   }
 
-  /**
-   * Crea una baraja rellenando el contenedor con instancias de la clase carta.
-   */
   barajar(){
     let palo = ["T", "P", "C", "R"];
     let carta;
     palo.forEach(p =>{
       for(let j=1; j<=13; j++){
         carta = new Carta(j, p)
-        this.baraja.push(carta);         //Se añade la carta.
+        this.baraja.push(carta);
       }
-    });    //Cada palo se combina con los números del 1 al 13.
+    });
+    this.baraja = _.shuffle(this.baraja);
   }
 
-  /**
-   * Retira una carta al azar de la baraja (y la elimina de ésta).
-   * @return {Carta} la carta retirada.
-   */
-  getCarta(){
-    let carta = new Carta();
-    let indice_aleatorio = (Math.floor(Math.random()*this.baraja.length));
-    carta = this.baraja[indice_aleatorio];
-    this.baraja.splice(indice_aleatorio, 1);
+  getCarta(){  //Devuelve y elimina una carta de la baraja.
+    let carta = this.baraja.pop();
     return carta;
   }
-
 }
 
-/**
- * Clase que representa un jugador de blackjack.
- */
 class Jugador{
-
-  /**
-   * Crea un jugador a partir de un nombre.
-   * @param {String} nombre Nombre del jugador a crear.
-   */
   constructor(nombre){
     this.nombre = nombre;
-    this.mano = [];             //Almacena las cartas que vaya recibiendo el jugador.
-    this.plantado = false;      //Si el jugador se planta, cambia a true y deja de recibir cartas.
-    this.puntuacion = 0;        //Almacena la suma del valor de las cartas de mano.
+    this.mano = [];
+    this.plantado = false;     
+    this.puntuacion = 0;       
   }
 
-  /**
-   * Metodo que cambia el estado del jugador cuando no desea recibir más cartas.
-   */
   plantarse(){
     this.plantado = true;
   }
 
-  /**
-   * Metodo para agregar una carta a la mano del jugador.
-   * @param {Carta} carta la carta que se desea agregar a la mano.
-   */
   addCard(carta){
     this.mano.push(carta);
-    this.puntuacion += carta.valor;   //Cada vez que recibe una carta nueva, se suma su valor a puntuacion
+    this.puntuacion += carta.valor;
     this.drawCard(carta);
   }
 
-  drawCard(carta){
+  drawCard(carta){  //Representa graficamente la carta modificando el html.
     let card = document.createElement("div");
     card.classList.add("carta");
     elems.manoJugador.appendChild(card);
@@ -135,33 +98,16 @@ class Jugador{
   }
 }
 
-/**
- * Clase que representa un crupier
- * @extends Jugador
- */
 class Crupier extends Jugador{
-
-  /**
-   * Crea un Crupier
-   */
   constructor(){
-    super("Casa");  //El nombre del crupier siempre es "Casa".
-    this.jugadores = [];   //Almacena los jugadores que estén en la partida.
-
+    super("Casa");
+    this.jugadores = [];
   }
 
-  /**
-   * Metodo para agregar un jugador a la partida.
-   * @param {Jugador} jugador Jugador que desea unirse a la partida.
-   */
   addPlayer(jugador){
     this.jugadores.push(jugador);
   }
 
-  /**
-   * Metodo para repartir cartas a los jugadores que no estan plantados.
-   * @param {Baraja.baraja} baraja El array que contiene las cartas de la baraja que se usa para repartir.
-   */
   repartir(baraja){
     this.jugadores.forEach(j=>{
       if (!j.plantado){
@@ -171,16 +117,10 @@ class Crupier extends Jugador{
       this.addCard(baraja.getCarta());
     }
 
-  /**
-   * Metodo para comprobar si la partida ha terminado
-   */
-  check_status(){  //Llama a "fin_partida" si todos los jugadores y el crupier están plantados.
+  check_status(){
     elems.marcador.textContent = `${this.jugadores[0].nombre}: ${this.jugadores[0].puntuacion} - ${this.nombre}: ${this.puntuacion}`;
   }
 
-  /**
-   * Cuando la partida acaba, se comprueba qué jugador obtuvo una puntuacion mas cercana a 21.
-   */
   fin_partida(){
     this.jugadores.forEach(j=>{
       if(j.puntuacion >= 21 && this.puntuacion >= 21){
@@ -213,15 +153,9 @@ class Crupier extends Jugador{
 
     card.appendChild(textoSup);
     card.appendChild(textoInf);    
-
   }
-
-
 }
-
-
 //-------------------------------------------INICIO----------------------------------------------
-
 let baraja;
 let jugador1;
 let crupier;
@@ -253,8 +187,3 @@ function nuevaPartida(){
   elems.botonCarta.onclick = repartir;
   elems.botonPlantar.onclick = plantarse;
 }
-
-
-//Inicio partida
-
-
